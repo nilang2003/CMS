68d2b52f1f18c4bcb92b8d5afd1c31607ec6c6e9
Nilang Dave <110675577+nilang2003@users.noreply.github.com>
2024-05-29 08:16:05 +0530

    Create db.js

---
+++ Backend/db.js
@@ -0,0 +1,16 @@
+// For Database connection
+
+const mongoose = require("mongoose");
+const connString = "mongodb://localhost:27017/CMS";
+const connecToMongo = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(connString);
+    console.log("Connected to Mongo DB");
+  } catch (error) {
+    console.log("Error:", error);
+    process.exit();
+  }
+};
+
+module.exports = connecToMongo;
